74a4ecdceb466568faaa98cb3024b3ec20c850fb
ARAVINDHAN S <147696818+Aravindhangif@users.noreply.github.com>
2025-03-20 18:13:18 +0530

    Update url.js

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,2 +1,2 @@
 //export const baseUrl= "http://localhost:5000";
-export const baseUrl= "https://twitter-clone-b580.onrender.com";
+export const baseUrl= "https://twitter-clone-1-yqg2.onrender.com";
